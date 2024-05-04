d48b889c89e597f30d79d4bc42bfed86869e5fdb
tockn <tockn.jp@gmail.com>
2024-05-04 21:16:17 +0900

    :+1: use biome in example

--- example/prisma/it-runner/runner.ts
+++ example/prisma/it-runner/runner.ts
@@ -1,129 +1,148 @@
 import {
-  RecordSet,
-  RecordSetAssertion,
-  RecordSetAssertionSortKey,
-  applyRecordSet,
-  assertRecordSet,
-  autoComplete,
+	RecordSet,
+	RecordSetAssertion,
+	RecordSetAssertionSortKey,
+	applyRecordSet,
+	assertRecordSet,
+	autoComplete,
 } from "./engine";
 import { PrismaClient } from ".prisma/client";
 
 type RecursivePartial<K> = {
-  [attr in keyof K]?: K[attr] extends object ? RecursivePartial<K[attr]> : K[attr];
+	[attr in keyof K]?: K[attr] extends object
+		? RecursivePartial<K[attr]>
+		: K[attr];
 };
 
 export interface TestCase<Return> {
-  // State of DB records before test execution
-  recordSet: RecordSet;
+	// State of DB records before test execution
+	recordSet: RecordSet;
 
-  // The method under test
-  method: (db: PrismaClient) => Promise<Return>;
+	// The method under test
+	method: (db: PrismaClient) => Promise<Return>;
 
-  // The expected return value or a function that takes the expected return value as an argument.
-  returns?: RecursivePartial<Return>;
+	// The expected return value or a function that takes the expected return value as an argument.
+	returns?: RecursivePartial<Return>;
 
-  // A function to execute on the method's return value before comparison with "returns".
-  returnsPrehook?: (value: Return) => Promise<Return>;
+	// A function to execute on the method's return value before comparison with "returns".
+	returnsPrehook?: (value: Return) => Promise<Return>;
 
-  // snapshot test
-  snapshot?: boolean;
+	// snapshot test
+	snapshot?: boolean;
 
-  // Verify the "returns" result with assert. Can be used in conjunction with snapshots.
-  returnsAssert?: (ret: Return) => Promise<void>;
+	// Verify the "returns" result with assert. Can be used in conjunction with snapshots.
+	returnsAssert?: (ret: Return) => Promise<void>;
 
-  // The expected error
-  throws?: jest.Constructable;
+	// The expected error
+	throws?: jest.Constructable;
 
-  // Additional assertion
-  asserts?: (db: PrismaClient) => Promise<void>;
+	// Additional assertion
+	asserts?: (db: PrismaClient) => Promise<void>;
 
-  // The expected state of records after executing the method under test.
-  mutates?: RecordSetAssertion;
+	// The expected state of records after executing the method under test.
+	mutates?: RecordSetAssertion;
 
-  // Specify this when you want to compare records in the database after sorting them by a specific key.
-  // Keys are specified as an array, and if multiple keys are specified, comparison is done by sorting with compound keys in the specified order.
-  mutatesSortKey?: RecordSetAssertionSortKey;
+	// Specify this when you want to compare records in the database after sorting them by a specific key.
+	// Keys are specified as an array, and if multiple keys are specified, comparison is done by sorting with compound keys in the specified order.
+	mutatesSortKey?: RecordSetAssertionSortKey;
 }
 
-function assertRecords<Model>(expected: Model[], actual: Model[], sortKeys?: (keyof Model)[]) {
-  expect(actual.length).toEqual(expected.length);
-  (sortKeys ? sortRecords(actual, sortKeys) : actual).forEach((a, index) => {
-    expect(a).toMatchObject(expected[index] as any);
-  });
+function assertRecords<Model>(
+	expected: Model[],
+	actual: Model[],
+	sortKeys?: (keyof Model)[],
+) {
+	expect(actual.length).toEqual(expected.length);
+	(sortKeys ? sortRecords(actual, sortKeys) : actual).forEach((a, index) => {
+		expect(a).toMatchObject(expected[index] as any);
+	});
 }
 
 // Sorts the object array passed in "records" in ascending order according to the keys provided.
 // Keys are specified as an array, and if multiple elements are passed, sorting is done based on compound keys.
 // If null, it will be placed last (similar to PostgreSQL's sorting behavior).
 const sortRecords = <Model>(records: Model[], keys: (keyof Model)[]) => {
-  const buildCompareFn = (keyIdx: number): ((a: any, b: any) => number) => {
-    if (keys.length <= keyIdx) {
-      return (_: any, __: any) => 0;
-    }
-    const key = keys[keyIdx];
+	const buildCompareFn = (keyIdx: number): ((a: any, b: any) => number) => {
+		if (keys.length <= keyIdx) {
+			return (_: any, __: any) => 0;
+		}
+		const key = keys[keyIdx];
 
-    return (a: any, b: any) => {
-      if (a[key] != null && b[key] != null) {
-        if (a[key] > b[key]) {
-          return 1;
-        }
-        if (a[key] < b[key]) {
-          return -1;
-        }
-        return buildCompareFn(keyIdx + 1)(a, b);
-      }
-      if (a[key] != null) {
-        return -1;
-      }
-      if (b[key] != null) {
-        return 1;
-      }
-      return 0;
-    };
-  };
+		return (a: any, b: any) => {
+			if (a[key] != null && b[key] != null) {
+				if (a[key] > b[key]) {
+					return 1;
+				}
+				if (a[key] < b[key]) {
+					return -1;
+				}
+				return buildCompareFn(keyIdx + 1)(a, b);
+			}
+			if (a[key] != null) {
+				return -1;
+			}
+			if (b[key] != null) {
+				return 1;
+			}
+			return 0;
+		};
+	};
 
-  return records.sort(buildCompareFn(0));
+	return records.sort(buildCompareFn(0));
 };
 
-export function _TEST_runIntegrationTest<Return>(db: PrismaClient, t: TestCase<Return> | (() => TestCase<Return>)): jest.ProvidesCallback {
-  return async function () {
-    try {
-      const tt = typeof t === "function" ? t() : t;
+export function _TEST_runIntegrationTest<Return>(
+	db: PrismaClient,
+	t: TestCase<Return> | (() => TestCase<Return>),
+): jest.ProvidesCallback {
+	return async function () {
+		try {
+			const tt = typeof t === "function" ? t() : t;
 
-      const recordSet = autoComplete(tt.recordSet, {} /* WIP */);
+			const recordSet = autoComplete(tt.recordSet, {} /* WIP */);
 
-      await applyRecordSet(db, recordSet, {});
+			await applyRecordSet(db, recordSet, {});
 
-      if (tt.throws) {
-        await expect(tt.method(db)).rejects.toThrow(tt.throws);
-        if (tt.asserts) {
-          await tt.asserts(db);
-        }
-        if (tt.mutates) {
-          await assertRecordSet(db, tt.mutates, assertRecords, tt.mutatesSortKey);
-        }
-        return;
-      }
+			if (tt.throws) {
+				await expect(tt.method(db)).rejects.toThrow(tt.throws);
+				if (tt.asserts) {
+					await tt.asserts(db);
+				}
+				if (tt.mutates) {
+					await assertRecordSet(
+						db,
+						tt.mutates,
+						assertRecords,
+						tt.mutatesSortKey,
+					);
+				}
+				return;
+			}
 
-      const ret = await tt.method(db).then((value) => (tt.returnsPrehook ? tt.returnsPrehook(value) : value));
+			const ret = await tt
+				.method(db)
+				.then((value) =>
+					tt.returnsPrehook ? tt.returnsPrehook(value) : value,
+				);
 
-      if (tt.returns !== undefined) {
-        if (tt.returns === null) {
-          expect(ret).toBeNull();
-        } else {
-          expect(ret).toMatchObject(tt.returns);
-        }
-      }
-      if (tt.snapshot) expect(ret).toMatchSnapshot();
-      if (tt.returnsAssert) await tt.returnsAssert(ret);
-      if (tt.asserts) await tt.asserts(db);
-      if (tt.mutates) await assertRecordSet(db, tt.mutates, assertRecords, tt.mutatesSortKey);
-    } finally {
-      await db.$disconnect();
-    }
-  };
+			if (tt.returns !== undefined) {
+				if (tt.returns === null) {
+					expect(ret).toBeNull();
+				} else {
+					expect(ret).toMatchObject(tt.returns);
+				}
+			}
+			if (tt.snapshot) expect(ret).toMatchSnapshot();
+			if (tt.returnsAssert) await tt.returnsAssert(ret);
+			if (tt.asserts) await tt.asserts(db);
+			if (tt.mutates)
+				await assertRecordSet(db, tt.mutates, assertRecords, tt.mutatesSortKey);
+		} finally {
+			await db.$disconnect();
+		}
+	};
 }
 
 export const __test_only__ = {
-  sortRecords,
+	sortRecords,
 };
--- example/src/index.test.ts
+++ example/src/index.test.ts
@@ -1,30 +1,31 @@
-import {_TEST_runIntegrationTest} from "../prisma/it-runner/runner";
-import {PrismaClient} from ".prisma/client";
+import { _TEST_runIntegrationTest } from "../prisma/it-runner/runner";
+import { PrismaClient } from ".prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 describe("test", () => {
-  it("runner", _TEST_runIntegrationTest(prisma, {
-    recordSet: {
-      User: [
-        { id: "1", name: "naruto", email: "naruto@example.com" }
-      ]
-    },
-    method: async (prisma) => {
-      await prisma.user.create({
-        data: { id: "2", name: "sasuke", email: "sasuke@example.com" }
-      })
-      return prisma.user.findMany()
-    },
-    returns: [
-      { id: "1", name: "naruto", email: "naruto@example.com" },
-      { id: "2", name: "sasuke", email: "sasuke@example.com" }
-    ],
-    mutates: {
-      User: [
-        { id: "1", name: "naruto", email: "naruto@example.com" },
-        { id: "2", name: "sasuke", email: "sasuke@example.com" }
-      ]
-    }
-  }))
-})
+	it(
+		"runner",
+		_TEST_runIntegrationTest(prisma, {
+			recordSet: {
+				User: [{ id: "1", name: "naruto", email: "naruto@example.com" }],
+			},
+			method: async (prisma) => {
+				await prisma.user.create({
+					data: { id: "2", name: "sasuke", email: "sasuke@example.com" },
+				});
+				return prisma.user.findMany();
+			},
+			returns: [
+				{ id: "1", name: "naruto", email: "naruto@example.com" },
+				{ id: "2", name: "sasuke", email: "sasuke@example.com" },
+			],
+			mutates: {
+				User: [
+					{ id: "1", name: "naruto", email: "naruto@example.com" },
+					{ id: "2", name: "sasuke", email: "sasuke@example.com" },
+				],
+			},
+		}),
+	);
+});
